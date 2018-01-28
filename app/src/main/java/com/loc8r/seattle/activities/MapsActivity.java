7ca90d4286f8f223b5ade2b295a5faf4be6a93b3
szaske <github@zaske.com>
2018-01-28 01:04:13 -0800

    redesigning POI details view, and adding debug info view

--- app/src/main/java/com/loc8r/seattle/activities/MapsActivity.java
+++ app/src/main/java/com/loc8r/seattle/activities/MapsActivity.java
@@ -1,17 +1,11 @@
 package com.loc8r.seattle.activities;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.location.Location;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.LinearSnapHelper;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SnapHelper;
 
-import android.transition.Slide;
-import android.transition.TransitionManager;
 import android.animation.ValueAnimator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -32,42 +26,40 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.loc8r.seattle.R;
-import com.loc8r.seattle.adapters.POIMapRecyclerViewAdapter;
 import com.loc8r.seattle.interfaces.LocationListener;
 import com.loc8r.seattle.models.POI;
+import com.loc8r.seattle.mongodb.MongoDBManager;
 import com.loc8r.seattle.utils.StateManager;
 import com.mancj.slideup.SlideUp;
 import com.mancj.slideup.SlideUpBuilder;
 
 import android.view.View;
 
+import org.parceler.Parcels;
+
 import java.util.ArrayList;
 
 public class MapsActivity extends LoggedInActivity implements
-        POIMapRecyclerViewAdapter.ClickListener,
         GoogleMap.OnMarkerClickListener,
         OnMapReadyCallback {
 
     private static final String TAG = MapsActivity.class.getSimpleName();
     private GoogleMap mMap;
-    private Marker mDeviceMarker;
-    private Location mCurrentLocation;
-    private ArrayList<POI> listOfPOIs;
-    private ArrayList<Marker> listOfPOIMarkers = new ArrayList<Marker>();
-    private RecyclerView locationsRecyclerView;
-    private ViewPager mViewPager;
-    // private CardPagerAdapter mCardAdapter;
-    private POIMapRecyclerViewAdapter mapRVAdapter;
-    public ViewGroup transitionsContainer;
-    private View AnimeView;
-    private Animation slide_up_animation;
+    private ArrayList<POI> mListOfPOIs;
+    private ArrayList<Marker> mlistOfPOIMarkers = new ArrayList<Marker>();
+    public ViewGroup mDrawerContainer;
+    private Animation mDrawer_up_animation;
 
-    private int drawerClosedYDistance;
-    private SlideUp drawer;
-    private View drawerView;
-    private View hideImageView;
-    private Button detailButton;
-    private POI selectedPoi;
+    private SlideUp mDrawer;
+    private View mDrawerView;
+    private View mHideImageView;
+    private TextView mDrawerTitleTV;
+    private TextView mDrawerDescTV;
+    private Button mDrawerDetailButton;
+    private Location mCurrentLocation;
+
+    // The selected POI
+    private POI mSelectedPOI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,7 +74,7 @@ public class MapsActivity extends LoggedInActivity implements
         mapFragment.getMapAsync(this);
 
         // Get and remember the full list of POI objects
-        listOfPOIs = CreateDummyPOIList();
+        mListOfPOIs = MongoDBManager.getInstance(getApplicationContext()).CreateDummyPOIList();
 
         // Create RV outside of Map
         // setUpRecyclerViewOfLocationCards();
@@ -90,27 +82,18 @@ public class MapsActivity extends LoggedInActivity implements
         // Drawer Setup
         DrawerSetup();
 
-        Log.d(TAG, "transitionContainer child count is " + String.valueOf(transitionsContainer.getChildCount()));
+        // Change text in mDrawer to screen height
+        //setHeightText();
 
-        // Change text in drawer to screen height
-        setHeightText();
 
-        detailButton = findViewById(R.id.poi_details_button);
-        detailButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(MapsActivity.this, "Get details on", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
     }
 
     public void CreateAnimation(){
         // Load the animation from XML
-        slide_up_animation = AnimationUtils.loadAnimation(MapsActivity.this, R.anim.slide_up);
+        mDrawer_up_animation = AnimationUtils.loadAnimation(MapsActivity.this, R.anim.slide_up);
 
         // set the animation listener
-        slide_up_animation.setAnimationListener(new Animation.AnimationListener() {
+        mDrawer_up_animation.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
             }
@@ -118,7 +101,7 @@ public class MapsActivity extends LoggedInActivity implements
             @Override
             public void onAnimationEnd(Animation animation) {
 
-                if (animation == slide_up_animation) {
+                if (animation == mDrawer_up_animation) {
                     Toast.makeText(MapsActivity.this, "Animation Stopped", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -138,11 +121,27 @@ public class MapsActivity extends LoggedInActivity implements
     }
 
     private void DrawerSetup(){
-        //assign the drawer view
-        drawerView = findViewById(R.id.drawerView);
+        //assign the mDrawer view and other contained views
+        mDrawerView = findViewById(R.id.drawerView);
+        mDrawerTitleTV = findViewById(R.id.title_textView);
+        mDrawerDescTV = findViewById(R.id.desc_textView);
 
-        //create the drawer object
-        drawer = new SlideUpBuilder(drawerView)
+        // Configure the details button and click listener
+        mDrawerDetailButton = findViewById(R.id.poi_details_button);
+        mDrawerDetailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //let's go to the Details activity
+                Intent i = new Intent(MapsActivity.this, POIDetailActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("poi", Parcels.wrap(mSelectedPOI));
+                i.putExtras(bundle);
+                startActivity(i); // POI is now passed to the new Activity
+            }
+        });
+
+        //create the mDrawer SlideUp object
+        mDrawer = new SlideUpBuilder(mDrawerView)
                 .withListeners(new SlideUp.Listener.Events() {
                     @Override
                     public void onSlide(float percent) {
@@ -166,26 +165,14 @@ public class MapsActivity extends LoggedInActivity implements
                 .withSlideFromOtherView(findViewById(R.id.rootView))
                 .build();
 
-        hideImageView = findViewById(R.id.arrow_down_imageView);
-        hideImageView.setOnClickListener(new View.OnClickListener() {
+        mHideImageView = findViewById(R.id.arrow_down_imageView);
+        mHideImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                drawer.hide();
+                mDrawer.hide();
             }
         });
 
-        // Name the drawer
-        transitionsContainer = findViewById(R.id.mapDrawer);
-        // View slideView = findViewById(R.id.slideView);
-
-        //move it below the screen
-
-        transitionsContainer.setTranslationY((getScreenHeight()-50));
-    }
-
-    private void setHeightText(){
-        TextView tv = findViewById(R.id.heightTV);
-        tv.setText(String.valueOf(getScreenHeight()));
     }
 
     public static void move(final ViewGroup viewG){
@@ -241,17 +228,17 @@ public class MapsActivity extends LoggedInActivity implements
         mMap.setOnMarkerClickListener(this);
     }
 
-    public ArrayList<POI> CreateDummyPOIList(){
-        ArrayList<POI> tempPOIList = new ArrayList<POI>();
-
-        tempPOIList.add(new POI("Dick's",CreateLocation(47.661116, -122.327877),"Dicks", "www.zaske.com", "street art", "StampIS1","Stamp Text 1"));
-        tempPOIList.add(new POI("Sea Monster Lounge",CreateLocation(47.661542, -122.332299),"Sea Monster", "www.zaske2.com", "street art", "StampIS2","Stamp Text 2"));
-        tempPOIList.add(new POI("Library",CreateLocation(47.661173, -122.338994),"Library", "www.zaske3.com", "street art", "StampIS3","Stamp Text 3"));
-        tempPOIList.add(new POI("Portage Bay Cafe",CreateLocation(47.657846, -122.317634),"Famous eatery with a great waffle bar", "www.zaske3.com", "street art", "StampIS4","Stamp Text 4"));
-        tempPOIList.add(new POI("University Barbershop",CreateLocation(47.658945, -122.313323),"Seattle's best barbershop", "www.zaske3.com", "street art", "StampIS5","Stamp Text 5"));
-        tempPOIList.add(new POI("Burke Museum",CreateLocation(47.660704, -122.310510),"Famous natural history museum", "www.zaske3.com", "street art", "StampIS6","Stamp Text 6"));
-        return tempPOIList;
-    }
+//    public ArrayList<POI> CreateDummyPOIList(){
+//        ArrayList<POI> tempPOIList = new ArrayList<POI>();
+//
+//        tempPOIList.add(new POI("Dick's",CreateLocation(47.661116, -122.327877),"Dicks", "www.zaske.com", "street art", "StampIS1","Stamp Text 1"));
+//        tempPOIList.add(new POI("Sea Monster Lounge",CreateLocation(47.661542, -122.332299),"Sea Monster", "www.zaske2.com", "street art", "StampIS2","Stamp Text 2"));
+//        tempPOIList.add(new POI("Library",CreateLocation(47.661173, -122.338994),"Library", "www.zaske3.com", "street art", "StampIS3","Stamp Text 3"));
+//        tempPOIList.add(new POI("Portage Bay Cafe",CreateLocation(47.657846, -122.317634),"Famous eatery with a great waffle bar", "www.zaske3.com", "street art", "StampIS4","Stamp Text 4"));
+//        tempPOIList.add(new POI("University Barbershop",CreateLocation(47.658945, -122.313323),"Seattle's best barbershop", "www.zaske3.com", "street art", "StampIS5","Stamp Text 5"));
+//        tempPOIList.add(new POI("Burke Museum",CreateLocation(47.660704, -122.310510),"Famous natural history museum", "www.zaske3.com", "street art", "StampIS6","Stamp Text 6"));
+//        return tempPOIList;
+//    }
 
     // Create a location given a Lat & Long
     public Location CreateLocation(Double latitude, Double longitude){
@@ -261,93 +248,23 @@ public class MapsActivity extends LoggedInActivity implements
         return tempLocation;
     }
 
-    private void CreateListOfMarkers(){
-        for (int x = 0; x < listOfPOIs.size(); x++) {
-            POI singlePOI = listOfPOIs.get(x);
-
-            // Add the location's marker to the map
-            LatLng poiLatLng = new LatLng(singlePOI.getLatitude(), singlePOI.getLongitude());
-
-            Marker tempMarker = mMap.addMarker(new MarkerOptions()
-            .position(poiLatLng)
-            .title(singlePOI.getName()));
-
-            // Create a list of markers so we can address them when clicked (so we have an index of current markers)
-            listOfPOIMarkers.add(tempMarker);
-        }
-    }
-
     private LatLng locationToLatLong(Location location){
         return new LatLng(location.getLatitude(),location.getLongitude());
-    }
-
-    private void setUpRecyclerViewOfLocationCards() {
-        // Initialize the recyclerview of location cards and a custom class for automatic card scrolling
-        locationsRecyclerView = findViewById(R.id.map_rv);
-        locationsRecyclerView.setHasFixedSize(true);
-
-
-        // locationsRecyclerView.setLayoutManager(new LinearLayoutManagerWithSmoothScroller(this));
-        //locationsRecyclerView.setLayoutManager(new GridLayoutManager(this,2,GridLayoutManager.HORIZONTAL, false));
-        locationsRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
-        mapRVAdapter = new POIMapRecyclerViewAdapter(listOfPOIs,
-                getApplicationContext(), this, 3);
-        locationsRecyclerView.setAdapter(mapRVAdapter);
-        SnapHelper snapHelper = new LinearSnapHelper();
-
-        snapHelper.attachToRecyclerView(locationsRecyclerView);
-
-//        //Setup ViewPager
-//        mViewPager = findViewById(R.id.viewPager);
-//
-//        // Create the Card Adapter
-//        mCardAdapter = new CardPagerAdapter();
-//
-//        for (int x = 0; x < listOfPOIs.size(); x++) {
-//            mCardAdapter.addCardItem(listOfPOIs.get(x));
-//        }
-//
-//        //Assign the adapter
-//        mViewPager.setAdapter(mCardAdapter);
-
-    }
-
-    // Click event listener for the RV
-    @Override public void onItemClick(int position) {
-
-        // Get the selected individual location via its card's position in the recyclerview of cards
-        POI selectedPOICard = listOfPOIs.get(position);
-        Marker selectedPOIMarker = listOfPOIMarkers.get(position);
-
-        Toast.makeText(getApplicationContext(), "Card #" + position+ " clicked", Toast.LENGTH_SHORT).show();
-
-        // Zoom in to the linked marker
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(selectedPOIMarker.getPosition(), 16));
-
-        //scroll the recycler view
-        locationsRecyclerView.smoothScrollToPosition(position);
-
-        TransitionManager.beginDelayedTransition(transitionsContainer, new Slide(Gravity.BOTTOM));
-
-        // TODO: update below code to work with Google Maps
-        // Retrieve and change the selected card's marker to the selected marker icon
-//        Marker markerTiedToSelectedCard = mapboxMap.getMarkers().get(position);
-//        adjustMarkerSelectStateIcons(markerTiedToSelectedCard);
-
-        // Reposition the map camera target to the selected marker
-//        LatLng selectedPOILatLng = selectedPOICard.getLatLng();
-//        repositionMapCamera(selectedPOILatLng);
     }
 
     @Override
     public boolean onMarkerClick(final Marker marker) {
 
         // Assign the selected POI by marker
-        selectedPoi = listOfPOIs.get((int)marker.getTag());
+        mSelectedPOI = mListOfPOIs.get((int)marker.getTag());
 
-        Log.d(TAG, "onMarkerClick: You selected marker " + selectedPoi.getName() );
-        //Bring up the details drawer
-        drawer.show();
+        //Set draw info to selected POI
+        mDrawerTitleTV.setText(mSelectedPOI.getName());
+        mDrawerDescTV.setText(mSelectedPOI.getDescription());
+
+        Log.d(TAG, "onMarkerClick: You selected marker " + mSelectedPOI.getName() );
+        //Bring up the details mDrawer
+        mDrawer.show();
 
         // We return false to indicate that we have not consumed the event and that we wish
         // for the default behavior to occur (which is for the camera to move such that the
@@ -378,14 +295,15 @@ public class MapsActivity extends LoggedInActivity implements
                             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(locationToLatLong(location), 16));
                         }
 
-                        // Store returned location in the state Manager
+                        // Store returned location in the state Manager and member variable
                         StateManager.getInstance().setCurrentLocation(location);
+                        mCurrentLocation = location;
 
                         // clear all current markers on map
                         mMap.clear();
 
                         // Step through all POI's and show markers for close ones (400 meters)
-                        for (POI poi : listOfPOIs) {
+                        for (POI poi : mListOfPOIs) {
                             // if the POI is within 800 meters
                             if (poi.getDistance() < 800) {
                                 // Add the location's marker to the map
@@ -394,7 +312,7 @@ public class MapsActivity extends LoggedInActivity implements
                                 Marker tempMarker = mMap.addMarker(new MarkerOptions()
                                         .position(poiLatLng)
                                         .title(poi.getName()));
-                                tempMarker.setTag(listOfPOIs.indexOf(poi));
+                                tempMarker.setTag(mListOfPOIs.indexOf(poi));
 
                                 //log that the marker is displayed
                                 Log.d(TAG, "showing marker "+ poi.getName());
--- app/src/main/java/com/loc8r/seattle/mongodb/MongoDBManager.java
+++ app/src/main/java/com/loc8r/seattle/mongodb/MongoDBManager.java
@@ -1,6 +1,7 @@
 package com.loc8r.seattle.mongodb;
 
 import android.content.Context;
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -595,6 +596,30 @@ public class MongoDBManager {
             AllString+=poi.toString();
         }
         return AllString;
+    }
+
+    public ArrayList<POI> CreateDummyPOIList(){
+        ArrayList<POI> tempPOIList = new ArrayList<POI>();
+
+        tempPOIList.add(new POI("Dick's",CreateLocation(47.661116, -122.327877),"Since 1954, original of a chain dishing out burgers, fries & hand-dipped shakes (open late).", "https://www.zaske.com/loc8r/seattle_medal.jpg", "street art", "StampIS1","Stamp Text 1"));
+        tempPOIList.add(new POI("Sea Monster Lounge",CreateLocation(47.661542, -122.332299),"We feature the very best in Seattle live funk, soul and jazz music, blues/electronic, etc. Tuesday through Sunday.  and we have a covered outdoor patio! it's also a famous spot for musicians getting off gigs elsewhere and wanting to drop in late for a jam session (and some liquor) and you will know why the moment you walk in. Charlie Hunter, Macklemore, Alan Stone, Roy Hargrove, Blake Lewis, Kevin Sawka, etc... 10 NW beers on tap, house liquor infusions, snacks, games, fun!", "https://www.zaske.com/loc8r/seattle_needle.jpg", "street art", "StampIS2","Stamp Text 2"));
+        tempPOIList.add(new POI("Library",CreateLocation(47.661173, -122.338994),"The Wilmot Memorial Library opened Sept. 10, 1949, in a house donated by Alice Wilmot Dennis in memory of her sister, Florence Wilmot Metcalf. The branch moved to remodeled quarters in a former police and fire station in 1985 and was renamed the Wallingford-Wilmot Library.\n" +
+                "\n" +
+                " \n" +
+                "\n" +
+                "The branch shared its new space with the 45th Street Clinic, which eventually expanded and asked the library to relocate.", "https://www.zaske.com/loc8r/seattle_library.jpg", "street art", "StampIS3","Stamp Text 3"));
+        tempPOIList.add(new POI("Portage Bay Cafe",CreateLocation(47.657846, -122.317634),"A tasty place to go for an all-American, local/organic/sustainable-focused breakfast or brunch, including cage-free eggs from Olympia, hams and sausages made in-house with Carlton Farms pork, all-organic and local-when-possible veggies, and so forth. Reservations are highly recommended on the weekends.", "https://www.zaske.com/loc8r/seattle_skyline1.jpg", "street art", "StampIS4","Stamp Text 4"));
+        tempPOIList.add(new POI("University Barbershop",CreateLocation(47.658945, -122.313323),"A great neighborhood barbershop, serving 'The Ave' for over 75 years", "https://www.zaske.com/loc8r/seattle_market.jpg", "street art", "StampIS5","Stamp Text 5"));
+        tempPOIList.add(new POI("Burke Museum",CreateLocation(47.660704, -122.310510),"The Burke Museum of Natural History and Culture (Burke Museum) is a natural history museum in Seattle, Washington, in the United States. Established in 1899 as the Washington State Museum, it traces its origins to a high school naturalist club formed in 1879. The museum is the oldest natural history museum west of the Mississippi River and boasts a collection of more than 16 million artifacts, including the world's largest collection of spread bird wings. Located on the campus of the University of Washington, the Burke Museum is the official state museum of Washington.", "https://www.zaske.com/loc8r/seattle_library.jpg", "street art", "StampIS6","Stamp Text 6"));
+        return tempPOIList;
+    }
+
+    // Create a location given a Lat & Long
+    public Location CreateLocation(Double latitude, Double longitude){
+        Location tempLocation = new Location("");
+        tempLocation.setLongitude(longitude);
+        tempLocation.setLatitude(latitude);
+        return tempLocation;
     }
 
 } // end of Manager class
